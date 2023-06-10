f50d183d8c41df5b975298731eef87791395f060
sowon <thdnjs3596@gmail.com>
2023-06-11 03:31:01 +0900

    Feat #3 카카오 로그인 API 작업
    * 카카오 API 생성
    * 현재는 app에 작업했는데 다음 작업인 apple 소셜 로그인까지 끝나면 리팩토링 해야함
    * axios, config 설치
    * key정보들 따로 관리할 방안 찾아야함
    * 참고 : https://www.youtube.com/watch?v=E-a-wQqybbE&t=109s

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Response,
+  Request,
+  UnauthorizedException,
+  BadRequestException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +17,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/kakao-login')
+  async login(@Body() body: any, @Response() res): Promise<any> {
+    try {
+      // 카카오 토큰 조회 후 계정 정보 가져오기
+      const { code, domain } = body;
+      if (!code || !domain) {
+        throw new BadRequestException('카카오 정보가 없습니다.');
+      }
+      const kakao = await this.appService.kakaoLogin({ code, domain });
+
+      console.log(`kakaoUserInfo : ${JSON.stringify(kakao)}`);
+      if (!kakao.id) {
+        throw new BadRequestException('카카오 정보가 없습니다.');
+      }
+
+      res.send({
+        user: kakao,
+        message: 'success',
+      });
+    } catch (e) {
+      console.log(e);
+      throw new UnauthorizedException();
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // CORS 허용
+  app.enableCors({
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
+  });
+
+  // local 테스트 시 프론트 3000을 피해 3001번으로 열었음.
+  await app.listen(3001);
 }
 bootstrap();
