017f41cd0d4c0087ba6bf9ae0b279ead59776a16
ssowon <thdnjs3596@gmail.com>
2023-07-15 17:01:34 +0900

    Feat #8 - swagger 적용 * yaml to json 방식으로 swagger 적용

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import app from './loaders/express.js';
 import router from './src/routes/index.js';
-import { config, passport } from './loaders/module.js';
+import { config, passport, yaml, swaggerUI } from './loaders/module.js';
 
+const openAPIDocument = yaml.load('./api/openapi.yaml');
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openAPIDocument));
 app.use('/', router);
 
 app.listen(config.get('server.port'));
--- loaders/module.js
+++ loaders/module.js
@@ -1,4 +1,7 @@
 import config from 'config';
 import passport from 'passport';
+import yaml from 'yamljs';
+import swaggerUI from 'swagger-ui-express';
 
-export { config, passport };
+
+export { config, passport, yaml, swaggerUI };
